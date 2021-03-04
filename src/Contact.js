b4a5763034ed8a0b2f4e9bde42e55abd2a0a1732
m1k347 <66216436+m1k347@users.noreply.github.com>
2021-03-04 16:38:18 +0000

    linked-in added, about restyled

--- src/Contact.js
+++ src/Contact.js
@@ -3,6 +3,7 @@ import React from 'react';
 import phone from './phone.png';
 import github from './github.png';
 import email from './envelope.png';
+import linkedIn from './linked-in.png';
 
 function Contact() {
 
@@ -12,7 +13,7 @@ function Contact() {
                 <figure className="contactIconText">
                     <a className="anchorReset" href="https://github.com/MichaelWarr3n" target="_blank">
                         <img src={github} className="contactIcon" />
-                        <figcaption className="contactText bold">michaelwarren</figcaption>
+                        <figcaption className="contactText bold">michaelwarr3n</figcaption>
                     </a>
                 </figure>
                 <figure className="contactIconText">
@@ -25,6 +26,12 @@ function Contact() {
                     <a className="anchorReset" href="mailto:mjwarren96@yahoo.co.uk" target="_blank">
                         <img src={email} className="contactIcon" />
                         <figcaption className="contactText bold">mjwarren96@yahoo.co.uk</figcaption>
+                    </a>
+                </figure>
+                <figure className="contactIconText">
+                    <a className="anchorReset" href="https://www.linkedin.com/in/michael-warren-b03367176/" target="_blank">
+                        <img src={linkedIn} className="contactIcon" />
+                        <figcaption className="contactText bold">Michael Warren</figcaption>
                     </a>
                 </figure>
             </div>
